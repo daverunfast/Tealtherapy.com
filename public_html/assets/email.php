65bc8f0e7160f7b00979a386a9cd96e5b16d73f2
David Barrett <dab97@duke.edu>
2017-03-14 15:16:25 -0400

    as of 3.14.17

---
+++ public_html/assets/email.php
@@ -0,0 +1,16 @@
+<?php
+header('location:index.html');
+
+if($_POST["submit"]) {
+    $recipient="davidandrewbarrett@gmail.com";
+    $subject="Message submitted via TealTherapy.com";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\n Email: $senderEmail\n Message: $message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+}
+?>
